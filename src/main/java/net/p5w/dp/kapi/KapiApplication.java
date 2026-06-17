d101ff224f586d7e2891a79aedd5bb75472003b1
Skyle <toakai@163.com>
2026-06-17 21:01:18 +0800

    first commit

---
+++ src/main/java/net/p5w/dp/kapi/KapiApplication.java
@@ -0,0 +1,13 @@
+package net.p5w.dp.kapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/p5w/dp/kapi/KapiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.p5w.dp.kapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
