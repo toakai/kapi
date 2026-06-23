15ff751978f4b6bc5064cd5c83173cea845c1cb4
Skyle <toakai@163.com>
2026-06-23 16:47:58 +0800

    update

--- src/main/java/net/p5w/dp/kapi/KapiApplication.java
+++ src/main/java/net/p5w/dp/kapi/KapiApplication.java
@@ -1,9 +1,11 @@
 package net.p5w.dp.kapi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "net.p5w.dp.module")
 public class KapiApplication {
 
     public static void main(String[] args) {
